070ea81761ad2de92775ad2db2ef51f8a5704c84
buiduykhanh030401 <buiduykhanh.tdc2020@gmail.com>
2022-10-10 15:00:45 +0700

    change redux base to readux toolkit

--- client/src/components/ProvinceList.js
+++ client/src/components/ProvinceList.js
@@ -1,60 +1,52 @@
-import React, {useState,useEffect} from 'react';
-import {View, StyleSheet} from 'react-native';
+import React, { useState, useEffect } from 'react';
 import DropDownPicker from 'react-native-dropdown-picker';
 import axios from 'axios';
-import { useDispatch, useSelector} from 'react-redux';
-import { POSITION_SELECTED } from '../redux/reducers/infoReducer';
+import { useDispatch } from 'react-redux';
+import { getMapSelectedSuccess } from '../redux/mapSlice';
 
 const ProvinceList = () => {
-    const dispatch = useDispatch();
-    const [categoriesProvince, setCategoriesProvince] = useState([]);
-    const [open, setOpen] = useState(false);
-    const [value, setValue] = useState(null);
-    const [items, setItems] = useState([
-      {label: 'Apple', value: 'apple'},
-      {label: 'Banana', value: 'banana'}
-    ]);
+  const dispatch = useDispatch();
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(null);
+  const [items, setItems] = useState([
+    { label: 'Apple', value: 'apple' },
+    { label: 'Banana', value: 'banana' }
+  ]);
 
-    const setItemCategory = (data)=>{
-        let categories = [];
-        data.forEach(element => {
-            categories.push({
-                label: element.name,
-                value: element.name
-            })
-        });
-        setItems(categories);
-    }
+  const setItemCategory = (data) => {
+    let categories = [];
+    data.forEach(element => {
+      categories.push({
+        label: element.name,
+        value: element.name
+      })
+    });
+    setItems(categories);
+  }
 
-    //get category Province
-    useEffect(() => {
-        axios.get(`https://provinces.open-api.vn/api/`)
-            .then(res => {
-                const itemCategories = res.data;
-                setItemCategory(itemCategories);
-            })
-            .catch(error => console.log(error));
-    }, []);
-  
-    return (
-      <DropDownPicker
-        open={open}
-        value={value}
-        items={items}
-        setOpen={setOpen}
-        setValue={setValue}
-        setItems={setItems}
-        onSelectItem={(item) => {
-            dispatch({
-                type: POSITION_SELECTED,
-                item: item
-            })
-          }}
-        autoScroll={true}
-      />
-    );
+  //get category Province
+  useEffect(() => {
+    axios.get(`https://provinces.open-api.vn/api/`)
+      .then(res => {
+        const itemCategories = res.data;
+        setItemCategory(itemCategories);
+      })
+      .catch(error => console.log(error));
+  }, []);
+
+  return (
+    <DropDownPicker
+      open={open}
+      value={value}
+      items={items}
+      setOpen={setOpen}
+      setValue={setValue}
+      setItems={setItems}
+      onSelectItem={(item) => {
+        dispatch(getMapSelectedSuccess(item.value))
+      }}
+      autoScroll={true}
+    />
+  );
 }
-
-const styles = StyleSheet.create({})
-
 export default ProvinceList;
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,41 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import reducer from './reducers';
-// Redux-thunk cho phép sử dụng các tác dụng bất đồng bộ
-const middleware = [thunk];
-export const store = createStore(
-    reducer,
-    applyMiddleware(...middleware)
-)
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage'
+// import AsyncStorage from '@react-native-async-storage/async-storage';
+// import AsyncStorage from '@react-native-community/async-storage';
+import mapReducer from './mapSlice';
+
+import createWebStorage from "redux-persist/lib/storage/createWebStorage";
+
+
+const persistConfig = {
+    key: 'root',
+    storage
+}
+// REDUCER
+const rootReducer = combineReducers({
+    map: mapReducer
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
+})
+
+export let persistor = persistStore(store)
