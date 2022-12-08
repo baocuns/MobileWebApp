f35696f86b837e0919f64e717766d7f68b77966c
Bao <baonv.23.student@fit.tdc.edu.vn>
2022-12-08 18:32:09 +0700

    create: - checkout - payment update: - back arrow - cart - rootnav

--- client/src/screens/Cart.js
+++ client/src/screens/Cart.js
@@ -9,6 +9,7 @@ import {
   TouchableOpacity,
   Alert,
   ActivityIndicator,
+  ToastAndroid,
 } from 'react-native';
 import React, { useEffect, useState } from 'react';
 
@@ -19,8 +20,15 @@ import axios from 'axios';
 import FastImage from 'react-native-fast-image';
 import { PhotoRoute } from '../utils/constant';
 import { formatVND } from '../utils/function';
+import moment from 'moment'
+import { Checkbox } from 'react-native-paper';
+import { useNavigation } from '@react-navigation/native';
+import Loader from './Loader/Loader';
+
+
 
 const Cart = () => {
+  const navigation = useNavigation()
   const user = useSelector(state => state.auth.login.currentUser);
   const [carts, setCarts] = useState([]);
   const [isFetching, setIsFetching] = useState(false);
@@ -50,7 +58,7 @@ const Cart = () => {
   const sumTotal = () => {
     let sum = 0;
     carts.map((cart) => {
-      sum += cart.price;
+      sum += cart.sale;
     })
     return sum;
   }
@@ -88,11 +96,68 @@ const Cart = () => {
 
   }, [])
 
+  const [total, setTotal] = useState(0)
+  const [items, setItems] = useState([])
+  const [isLoader, setIsLoader] = useState(false)
+  const handleChangeItems = (value) => {
+    var it = items
+    if (it.some(e => e === value)) {
+      it = it.filter(e => e !== value)
+    } else {
+      it.push(value)
+    }
+    // total
+    var _total = 0
+    carts && carts.map(tour => {
+      if (it.some(e => e === tour._id)) {
+        _total += tour.sale
+      }
+    })
+
+    setItems(it)
+    setTotal(_total)
+  }
+
+  const handleCreateOrder = () => {
+    if (items.length > 0) {
+      ToastAndroid.show("Hệ thống đang tạo đơn hàng !", ToastAndroid.SHORT);
+      setIsLoader(true)
+      const data = {
+        items: items
+      }
+      axios.post(`https://api.travels.games/api/v1/order/create`, data, {
+        headers: {
+          "token": `Travel ${user?.accessToken}`,
+          "_id": user?._id,
+        }
+      })
+        .then(res => {
+          setIsLoader(false)
+          ToastAndroid.show("Hệ thống tạo đơn hàng thành công !", ToastAndroid.SHORT);
+          const { _id } = res.data.data
+          // console.log('data:', res.data.data);
+          navigation.navigate('Checkout', { oid: _id })
+        })
+        .catch(err => {
+          setIsLoader(false)
+          const { data } = err?.response
+          // console.log(data);
+          ToastAndroid.show("Hệ thống tạo đơn hàng thất bại !", ToastAndroid.SHORT);
+        })
+    } else {
+      ToastAndroid.show("Vui lòng chọn dịch vụ !", ToastAndroid.SHORT);
+    }
+  }
+
   return (
     <View
       style={{
         flex: 1,
       }}>
+      {isLoader && (
+        <Loader />
+      )}
+      <BackArrow name="Giỏ hàng" />
       <ScrollView
         style={{
           flex: 0.9,
@@ -101,107 +166,114 @@ const Cart = () => {
           style={{
             paddingLeft: 10,
           }}>
-          <BackArrow name="Giỏ hàng" />
 
-
-          {/* hình ảnh */}
-          {carts.map((cart, index) => {
-            return (
-              <View>
-                <View
-                  style={{
-                    flexDirection: 'row',
-
-                    alignSelf: 'center',
-                  }}>
+          <View className='mt-8'>
+            {/* hình ảnh */}
+            {carts.map((cart, index) => {
+              return (
+                <View key={index} className='border border-gray-200 p-2 m-2 drop-shadow-lg'>
                   <View
                     style={{
-                      paddingLeft: 20,
-                      paddingTop: 5,
-                    }}>
-                    <FastImage
-                      style={{
-                        width: 70,
-                        height: 70,
-                        borderRadius: 10,
-                      }}
-                      source={{ uri: PhotoRoute + cart.images[0] }}
-                    />
-                  </View>
+                      flexDirection: 'row',
 
-                  <View
-                    style={{
-                      paddingLeft: 20,
-                      marginRight: 10,
+                      alignSelf: 'center',
                     }}>
+                    <View className='pt-2 pl-2'>
+                      <Checkbox
+                        status={items.some(e => e === cart._id) ? 'checked' : 'unchecked'}
+                        onPress={() => handleChangeItems(cart._id)}
+                        color={'red'}
+                      />
+                    </View>
+
                     <View
                       style={{
-                        width: 250,
+                        paddingLeft: 10,
+                        paddingTop: 5,
                       }}>
-                      <Text
-                        numberOfLines={2}
-                        style={{
-                          color: 'black',
-                          fontWeight: 'bold',
-                          fontSize: 17,
-                          paddingBottom: 5,
-                        }}>
-                        {cart.title}
-                      </Text>
+                      <FastImage
+                        className='h-12 w-12 rounded'
+                        source={{ uri: PhotoRoute + cart.images[0] }}
+                      />
                     </View>
-                    <View>
-                      <Text style={{ color: 'black', paddingBottom: 5 }}>
-                        {formatVND(cart.price)}
-                      </Text>
-                    </View>
-                    <View>
-                      <Text style={{ color: 'black', paddingBottom: 5 }}>
-                        Ngày đi: 23/9/2022
-                      </Text>
-                    </View>
-                    <View>
-                      <Text style={{ color: 'orange' }}>Dịch vụ không hổ trợ</Text>
-                    </View>
-                  </View>
-                </View>
-                <View>
-                  <View>
-                    <TouchableOpacity
-                      onPress={() => {
-                        Alert.alert(
-                          "Travel app",
-                          "Hey guys, are you sure ?",
-                          [
-                            {
-                              text: "Cancel",
-                              onPress: () => console.log("Cancel Pressed"),
-                              style: "cancel"
-                            },
-                            {
-                              text: "Xóa", onPress: () => {
-                                removeToCart(cart._id)
-                              }
-                            },
-                          ]
-                        );
-                      }}
+
+                    <View
                       style={{
-                        alignItems: 'flex-end',
-                        marginRight: 20,
+                        paddingLeft: 20,
+                        marginRight: 10,
                       }}>
-                      <Text
+                      <View
                         style={{
-                          color: 'black',
-                          textDecorationLine: 'underline',
+                          width: 250,
                         }}>
-                        Delete
-                      </Text>
-                    </TouchableOpacity>
+                        <Text
+                          numberOfLines={2}
+                          style={{
+                            color: 'black',
+                            fontWeight: 'bold',
+                            fontSize: 17,
+                            paddingBottom: 5,
+                          }}>
+                          {cart.title}
+                        </Text>
+                      </View>
+                      <View>
+                        <Text style={{ color: 'black', paddingBottom: 5 }} className='line-through'>
+                          {formatVND(cart.price)}
+                        </Text>
+                        <Text style={{ color: 'black', paddingBottom: 5 }}>
+                          {formatVND(cart.sale)}
+                        </Text>
+                      </View>
+                      <View>
+                        <Text style={{ color: 'black', paddingBottom: 5 }}>
+                          Ngày đi: {moment(cart.time_start).format('DD-MM-yyyy')}
+                        </Text>
+                      </View>
+                      <View>
+                        <Text style={{ color: 'orange' }}>Dịch vụ không hổ trợ</Text>
+                      </View>
+                    </View>
+                  </View>
+                  <View>
+                    <View>
+                      <TouchableOpacity
+                        onPress={() => {
+                          Alert.alert(
+                            "Travel app",
+                            "Hey guys, are you sure ?",
+                            [
+                              {
+                                text: "Cancel",
+                                onPress: () => console.log("Cancel Pressed"),
+                                style: "cancel"
+                              },
+                              {
+                                text: "Xóa", onPress: () => {
+                                  removeToCart(cart._id)
+                                }
+                              },
+                            ]
+                          );
+                        }}
+                        style={{
+                          alignItems: 'flex-end',
+                          marginRight: 20,
+                        }}>
+                        <Text
+                          style={{
+                            color: 'black',
+                            textDecorationLine: 'underline',
+                          }}>
+                          Delete
+                        </Text>
+                      </TouchableOpacity>
+                    </View>
                   </View>
                 </View>
-              </View>
-            )
-          })}
+              )
+            })}
+          </View>
 
         </View>
         {isFetching && <ActivityIndicator size="large" color="#0000ff" />}
@@ -229,7 +301,7 @@ const Cart = () => {
                 paddingBottom: 10,
                 color: 'gray',
               }}>
-              Tổng cộng (0 đơn vị)
+              Tổng cộng ({items.length} đơn vị)
             </Text>
             <Text
               style={{
@@ -237,7 +309,7 @@ const Cart = () => {
                 fontWeight: 'bold',
                 fontSize: 15,
               }}>
-              ₫ {sumTotal()}
+              ₫ {total > 0 ? total : sumTotal()}
             </Text>
           </View>
           <View
@@ -245,6 +317,7 @@ const Cart = () => {
               justifyContent: 'flex-end',
             }}>
             <TouchableOpacity
+              onPress={handleCreateOrder}
               style={{
                 backgroundColor: 'orange',
                 width: 100,
@@ -257,7 +330,7 @@ const Cart = () => {
                 style={{
                   color: 'white',
                 }}>
-                Thanh Toán
+                Tạo Đơn Hàng
               </Text>
             </TouchableOpacity>
           </View>
